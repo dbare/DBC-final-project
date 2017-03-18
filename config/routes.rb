1ade174bc12bde6663b21a915f1461f19e49b68a
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-03-18 13:09:03 -0700

    adds custom routes for team, about, and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 	resources :users
 	resources :projects
 
+	get '/team' => 'team#index'
+	get '/contact' => 'contact#index'
+
 	get '/login' => 'sessions#new'
 	post '/login' => 'sessions#create'
 	get '/logout' => 'sessions#destroy'
