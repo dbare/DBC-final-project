0ee1e10fa7e013991bb065bd62e91baf1f67771d
Jack Howerton <jackhowerton@gmail.com>
2017-03-17 18:54:56 -0700

    add title to projects, update seed file accordingly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318004440) do
+ActiveRecord::Schema.define(version: 20170318015050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170318004440) do
     t.integer  "company_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "title"
     t.index ["company_id"], name: "index_projects_on_company_id", using: :btree
   end
 
