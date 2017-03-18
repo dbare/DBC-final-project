32a1b7144c2c5f67584348c18d944c2793d024b1
Jack Howerton <jackhowerton@gmail.com>
2017-03-18 16:08:29 -0700

    profile tweaks

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,24 @@
 class ProfilesController < ApplicationController
 
 	def new
-		@profile = Profile.new
+		# @profile = Profile.new
 	end
 
 	def create
-		
+		@profile = Profile.new(profile_params)
+		if @profile.save
+
+		else
+
+		end
+
 	end
+
+
+private
+
+def profile_params
+	params.require(:profile).permit(:location, :objective, :summary, :user_id)
+end
 
 end
