402b31dba619cc1359b27b2e04dc83b5b0bef687
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-03-17 19:37:21 -0700

    adds forms for adding new job posting, company id required to move forward

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,22 +1,49 @@
 class JobsController < ApplicationController
 
+	before_action :find_job, except: [:index, :new, :create]
+
 	def index
 		@jobs = Job.all 
 	end
 
-	def new 
+	def new
+		@job = Job.new
 	end 
-
-	def edit 
-	end
 
 	def create
-	end  
+		@job = Job.new(job_params)
+		if @job.save
+			redirect_to @job 
+		else
+			render :new
+		end 
+	end
 
-	def update 
+	def edit
+	  find_job
+	end
+
+	def show
+		find_job
 	end 
 
-	def destroy 
+
+	def update
+		find_job
+	end 
+
+	def destroy
+		find_job 
+	end 
+
+	private 
+
+	def find_job
+		@job = @job.find(params[:id])
+	end 
+
+	def job_params
+		params.require(:job).permit(:position, :location, :description)
 	end  
 
 end
