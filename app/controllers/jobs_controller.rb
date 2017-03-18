67d5efae9e40748e8512fb32308446905a48baeb
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-03-17 18:47:19 -0700

    add actions to jobs controller and defines jobs resource in routes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,22 @@
 class JobsController < ApplicationController
+
+	def index
+		@jobs = Job.all 
+	end
+
+	def new 
+	end 
+
+	def edit 
+	end
+
+	def create
+	end  
+
+	def update 
+	end 
+
+	def destroy 
+	end  
+
 end
