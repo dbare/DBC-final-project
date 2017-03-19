8ad9deaa5d4e89e525566a0d612863d14a60302b
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-03-19 14:07:01 -0700

    add welcome email content for preview

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,8 @@
 class UserMailer < ApplicationMailer
 	default from: "codeconnectgroup@gmail.com"
+
+	def welcome_email(user)
+		@user = user
+		mail(to: @user.email, subject: 'Welcome To Code Connect')
+	end 
 end
