12ca2e0271864b6bb22d03368aa428548c2b612f
Brian Bernheim <btbernheim@gmail.com>
2017-03-20 16:11:59 -0700

    Get carrier wave to work for resume creation, add more whitelist file types to photo uploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :photo, :company_id)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :photo, :company_id)
 	end
 
 end
