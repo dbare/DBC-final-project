5afc348142a5a3645aee1da893b67b5480393eef
Jack Howerton <jackhowerton@gmail.com>
2017-03-20 17:22:38 -0700

    favorite jobs on users own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,8 @@ class UsersController < ApplicationController
 			login
 			redirect_to @user
 		else
-			p "ELSE"
+			p "*" * 100
+			p "ELSE WE FUCKED UP"
 			render 'new'
 		end
 	end
@@ -52,7 +53,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :photo, :company_id)
+		params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :photo, :company_id, :unique_token)
 	end
 
 end
